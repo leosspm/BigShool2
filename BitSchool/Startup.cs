557091e20db1e97eb092d8ea827a3f3dc9f98561
Nguyễn Bửu Sơn <leoss1047@gmail.com>
2019-04-07 10:56:10 +0700

    Add project files.

---
+++ BitSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BitSchool.Startup))]
+namespace BitSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
